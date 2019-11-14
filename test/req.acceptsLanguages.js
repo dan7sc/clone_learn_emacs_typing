b47e1e2c2e4a90d668831d0d9e724fc0ae0858cc
daniel sc <su7cheng@gmail.com>
2019-11-14 16:26:32 -0200

    Test: req.acceptsLanguages.js

---
+++ test/req.acceptsLanguages.js
@@ -0,0 +1,52 @@
+const express = require('../');
+const request = require('supertest');
+
+describe('req', function() {
+    describe('.acceptsLanguages', function() {
+	it('should be true if language accepted', function(done) {
+	    let app = express();
+
+	    app.use(function(req, res) {
+		req.acceptsLanguages('en-us').should.be.ok();
+		req.acceptsLanguages('en').should.be.ok();
+		res.end();
+	    });
+
+	    request(app)
+		.get('/')
+		.set('Accept-Language', 'en;q=.5, en-us')
+		.expect(200, done);
+	});
+
+	it('should be false if language not accept', function(done) {
+	    let app = express();
+
+	    app.use(function(req, res) {
+		req.acceptsLanguages('es').should.not.be.ok();
+		res.end();
+	    });
+
+	    request(app)
+		.get('/')
+		.set('Accept-Language', 'en;q=.5, en-us')
+		.expect(200, done);
+	});
+
+	describe('when Accept-Language is not present', function() {
+	    it('should always return true', function(done) {
+		let app = express();
+
+		app.use(function(req, res) {
+		    req.acceptsLanguages('en').should.be.ok();
+		    req.acceptsLanguages('es').should.be.ok();
+		    req.acceptsLanguages('jp').should.be.ok();
+		    res.end();
+		});
+
+		request(app)
+		    .get('/')
+		    .expect(200, done);
+	    });
+	});
+    });
+});
