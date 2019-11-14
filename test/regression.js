5695f070530272c7e3b11d49985bfd0b126415a1
daniel sc <su7cheng@gmail.com>
2019-11-14 15:10:34 -0200

    Test: regression.js

---
+++ test/regression.js
@@ -0,0 +1,18 @@
+const express = require('../');
+const request = require('supertest');
+
+describe('throw after .end()', function() {
+    it('should fail gracefully', function(done) {
+	let app = express();
+
+	app.get('/', function(req, res) {
+	    res.end('yay');
+	    throw new Error('boom');
+	});
+
+	request(app)
+	    .get('/')
+	    .expect('yay')
+	    .expect(200, done);
+    });
+});
