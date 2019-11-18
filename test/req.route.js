8106efb0eb7d0a8d55a250eafd99255a54a98709
daniel sc <su7cheng@gmail.com>
2019-11-18 17:57:20 -0200

    Test: req.route.js

---
+++ test/req.route.js
@@ -0,0 +1,24 @@
+const express = require('../');
+const request = require('supertest');
+
+describe('req', function() {
+    describe('.route', function() {
+	it('should be the executed Route', function(done) {
+	    const app = express();
+
+	    app.get('/user/:id/:op?', function(req, res, next) {
+		req.route.path.should.equal('/user/:id/:op?');
+		next();
+	    });
+
+	    app.get('/user/:id/edit', function(req, res) {
+		req.route.path.should.equal('/user/:id/edit');
+		res.end();
+	    });
+
+	    request(app)
+		.get('/user/12/edit')
+		.expect(200, done);
+	});
+    });
+});
