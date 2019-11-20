c4ad14776097aa62fdb1a12c832df173bf6e972a
daniel sc <su7cheng@gmail.com>
2019-11-20 12:32:27 -0200

    Test: res.jsonp.js

---
+++ test/support/utils.js
@@ -0,0 +1,59 @@
+
+/**
+ * Module dependencies.
+ * @private
+ */
+
+const assert = require('assert');
+const Buffer = require('safe-buffer').Buffer;
+
+/**
+ * Module exports.
+ * @public
+ */
+
+exports.shouldHaveBody = shouldHaveBody;
+exports.shouldNotHaveBody = shouldNotHaveBody;
+exports.shouldNotHaveHeader = shouldNotHaveHeader;
+
+/**
+ * Assert that a supertest response has a specific body.
+ *
+ * @param {Buffer} buf
+ * @returns {function}
+ */
+
+function shouldHaveBody (buf) {
+    return function (res) {
+	const body = !Buffer.isBuffer(res.body)
+	      ? Buffer.from(res.text)
+	      : res.body;
+	assert.ok(body, 'response has body');
+	assert.strictEqual(body.toString('hex'), buf.toString('hex'));
+    };
+}
+
+/**
+ * Assert that a supertest response does not have a body.
+ *
+ * @return {function}
+ */
+
+function shouldNotHaveBody () {
+    return function (res) {
+	assert.ok(res.text === '' || res.text === undefined);
+    };
+}
+
+/**
+ * Assert that a supertest response does not have a header.
+ *
+ * @param {string} header Header name to check
+ * @returns {function}
+ */
+
+function shouldNotHaveHeader (header) {
+    return function (res) {
+	assert.ok(!(header.toLowerCase() in res.headers), 'should not have header ' + header);
+    };
+}
