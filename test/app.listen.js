3441639c12aff1f8532252c982836729120247d7
daniel sc <su7cheng@gmail.com>
2019-12-02 11:41:39 -0200

    Test: app.listen.js

---
+++ test/app.listen.js
@@ -0,0 +1,16 @@
+const express = require('../');
+
+describe('app.listen()', function() {
+    it('should wrap with an HTTP server', function(done) {
+	const app = express();
+
+	app.del('/tobi', function(req, res) {
+	    res.end('deleted tobi');
+	});
+
+	const server = app.listen(9999, function() {
+	    server.close();
+	    done();
+	});
+    });
+});
