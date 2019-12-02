80170eca3077b1d6c0501b02deced7083b99ad80
daniel sc <su7cheng@gmail.com>
2019-12-02 10:51:16 -0200

    Test: app.del.js

---
+++ test/app.del.js
@@ -0,0 +1,16 @@
+const express = require('../');
+const request = require('supertest');
+
+describe('app.del()', function() {
+    it('should alias app.delete()', function(done) {
+	const app = express();
+
+	app.del('/tobi', function(req, res) {
+	    res.end('deleted tobi');
+	});
+
+	request(app)
+	    .del('/tobi')
+	    .expect('deleted tobi', done);
+    });
+});
