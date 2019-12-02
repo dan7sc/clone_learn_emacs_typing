bb045dc5f77da23ff750bb2be30ea71c94848893
daniel sc <su7cheng@gmail.com>
2019-12-02 11:15:15 -0200

    Test: app.head.js

---
+++ test/app.head.js
@@ -0,0 +1,67 @@
+const express = require('../');
+const request = require('supertest');
+const assert = require('assert');
+
+describe('HEAD', function() {
+    it('should default to GET', function(done) {
+	const app = express();
+
+	app.get('/tobi', function(req, res) {
+	    // send() detects HEAD
+	    res.send('tobi');
+	});
+
+	request(app)
+	    .head('/tobi')
+	    .expect(200, done);
+    });
+
+    it('should output the same headers as GET requests', function(done) {
+	const app = express();
+
+	app.get('/tobi', function(req, res) {
+	    // send() detects HEAD
+	    res.send('tobi');
+	});
+
+	request(app)
+	    .get('/tobi')
+	    .expect(200, function(err, res) {
+		if (err) return done(err);
+		const headers = res.headers;
+		request(app)
+		    .get('/tobi')
+		    .expect(200, function(err, res) {
+			if (err) return done(err);
+			delete headers.date;
+			delete res.headers.date;
+			assert.deepEqual(res.headers, headers);
+			done();
+		    });
+	    });
+    });
+});
+
+describe('app.head()', function() {
+    it('should override', function(done) {
+	const app = express();
+	let called;
+
+	app.head('/tobi', function(req, res) {
+	    called = true;
+	    res.end('');
+	});
+
+	app.get('/tobi', function(req, res) {
+	    assert(0, 'should not call GET');
+	    res.send('tobi');
+	});
+
+	request(app)
+	    .head('/tobi')
+	    .expect(200, function() {
+		assert(called);
+		done();
+	    });
+    });
+});
