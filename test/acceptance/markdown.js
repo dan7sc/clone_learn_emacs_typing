ab15e6c8e74b4543354e91dcb095cccc9cbf4eda
daniel sc <su7cheng@gmail.com>
2019-12-23 14:40:27 -0200

    Test: acceptance/markdown.js

---
+++ test/acceptance/markdown.js
@@ -0,0 +1,20 @@
+const app = require('../../examples/markdown');
+const request = require('supertest');
+
+describe('markdown', function() {
+    describe('GET /', function() {
+        it('should respond with html', function(done) {
+            request(app)
+                .get('/')
+                .expect(/<h1[^>]*>Markdown Example<\/h1>/, done);
+        });
+    });
+
+    describe('GET /fails', function() {
+        it('should respond with an error', function(done) {
+            request(app)
+                .get('/fail')
+                .expect(500, done);
+        });
+    });
+});
